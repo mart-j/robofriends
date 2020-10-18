e7a324298463db66b79696a1e5be74ae65a97af1
Martins <m.jukss@gmail.com>
2020-10-18 15:47:00 +0300

    Added some style to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,11 +10,13 @@ const Card = ({ users }) => {
 return (
     users.map(user => {
         return (
-            <div className="container">
+            <div key={user.id} className="container" >
                 <img src={`https://robohash.org/${user.id}?size=200x200`}
                     alt="this is an robot" />
+                    <div>
                 <h1>{user.name}</h1>
                 <p>{user.email}</p>
+                </div>
             </div>
         )
     })
--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ class App extends Component {
 
 
 
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
